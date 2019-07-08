8f1eb67592c901713f8b8995fd0d8b2155b1229f
Rajarshi Ghoshal <rajarshi.ghoshal2013@gmail.com>
2019-07-09 01:30:28 +0530

    Add files via upload

---
+++ world_db_question.sql
@@ -0,0 +1,29 @@
+use world;
+show tables;
+describe city;
+describe country;
+describe countrylanguage;
+
+# 1. count of cities
+
+# 2. Country having highest number of cities
+
+# 3. Which language is its country’s official language and spoken by 80 to 90 percent of people
+
+# 4. In India, how many cities have been listed in “Maharashtra” district?
+
+# 5. Which country has the maximum population (if population is taken as the population from the country table)
+
+# 6. Which language is spoken in the maximum number of countries?
+
+# 7. Among the following, which language is the official language of the more number of countries?
+
+# 8. How many cities in North America are there where English is the official language
+
+# 9. Which city has the maximum population among these
+
+# 10. How many row entries are there with any value in the country table being NA
+
+# 11. How many countries are there whose name starts with I and ends with A
+
+# 12. Which continent has least surface area
